d6977b68cd8e5f26ead5f3104a54f6e99b351862
SkeepyC <63789123+SkeepyC@users.noreply.github.com>
2020-04-16 15:55:44 +0200

    Add files via upload

---
+++ cc_morpion-.py
@@ -0,0 +1,12 @@
+﻿a='[" "," "," "]'
+print(a)
+print(a)
+print(a)
+for i in range(9):
+    if i%2==0:
+        symbol="X"
+    else:
+        symbol="O"
+while (0<=X<=2) or (0<=Y<=2):
+        X=(input("X ?"))
+        Y=(input("Y ?"))
